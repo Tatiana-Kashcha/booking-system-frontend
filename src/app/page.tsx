8f52dddfa2becfb4f679d180ec478cd574181b7d
Tatiana Kashcha <tkashcha@gmail.com>
2025-09-25 00:07:07 +0300

    Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
 import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}></main>
-    </div>
+    <main>
+      <section>
+        <div className={styles.container}>
+          <div className={styles.thumb}>
+            <Image
+              src="/images/Booking.png"
+              alt="photo"
+              className={styles.photo}
+              width={300}
+              height={300}
+              priority
+            />
+          </div>
+          <h1 className={styles.title}>Welcome to Booking System !</h1>
+          <div className={styles.buttons}>
+            <Link href="/register" className={styles.button}>
+              Register
+            </Link>
+            <Link href="/login" className={styles.button}>
+              Login
+            </Link>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
