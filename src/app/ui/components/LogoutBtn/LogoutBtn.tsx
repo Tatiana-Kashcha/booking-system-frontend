9a19778a77184738a67acfb1d32b2952f75a09e6
Tatiana Kashcha <tkashcha@gmail.com>
2025-09-27 13:44:21 +0300

    update logout

--- src/app/ui/components/LogoutBtn/LogoutBtn.tsx
+++ src/app/ui/components/LogoutBtn/LogoutBtn.tsx
@@ -1,26 +1,33 @@
 "use client";
 
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { logOut } from "@/app/actions/auth";
 import styles from "./LogoutBtn.module.css";
 
 export const LogoutBtn = () => {
+  const [isDisabled, setIsDisabled] = useState(false);
+
   const router = useRouter();
 
   const handleLogout = async () => {
     try {
+      setIsDisabled(true);
       await logOut();
       localStorage.removeItem("authToken");
       router.push("/");
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsDisabled(false);
     }
   };
 
   return (
     <button
-      className={styles.button_logout}
       type="button"
+      className={styles.button_logout}
+      disabled={isDisabled}
       onClick={handleLogout}
     >
       Logout {"[---"}
